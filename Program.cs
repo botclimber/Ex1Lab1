8785550b71d300abfaa5c76d18f2efd769223c16
Daniel <daniel.silva.prg@gmail.com>
2019-04-04 10:56:10 +0100

    Updated with comment lines

--- Program.cs
+++ Program.cs
@@ -7,10 +7,6 @@
  * */
 
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Ex1Lab1
 {
@@ -18,43 +14,68 @@ namespace Ex1Lab1
     {
         static void Main(string[] args)
         {
+            #region variaveis
+            /// <summary>
+            /// Declaração de variaveis
+            /// <para><c>cont int Max = 27</c> valor maximo aleatorio</para>
+            /// </summary>
             const int MAX = 27;
             int[] arr = new int[MAX];
             int nrContent = 0;
             string msg;
+            #endregion
 
+            #region Interação com utilizador
             Console.WriteLine(" [-1] para sair: \n");
             for(nrContent = 0; nrContent < MAX; nrContent++)
             {
                 Console.WriteLine("Insira um numero: ");
+
+                /// <summary>
+                /// <c>int.Parse</c> Converte string em inteiro
+                /// </summary>
                 arr[nrContent] = int.Parse(Console.ReadLine());
 
+                /// <para>condição de paragem</para>
                 if (arr[nrContent] < 0) break;
             }
+            #endregion
 
+            #region Conclusão
             if (Simetric(arr, nrContent) == true)
                 msg = "é Simétrico";
             else
                 msg = "não é Simétrico";
 
             Console.WriteLine("O Array gerado "+msg);
+            #endregion
 
             Console.ReadKey();
 
         }
 
+        #region Metodo Verifica Simetrico
+        /// <summary>
+        ///    Metodo que verifica a simetria do array
+        /// </summary>
+        /// <param name="arr"> Array de int valores a receber</param>
+        /// <param name="tot"> Numero total de valores int no array</param>
+        /// <returns>bool</returns>
+
         public static bool Simetric(int[] arr, int tot)
         {
-            /*   */
+            // divide array
             int half = tot / 2;
 
             for (int i = 1; (i-1) <= half; i++)
             {
+                // compara array divido posicao (i-1) com a posicao (tot-i)
                 if (arr[i - 1] != arr[tot - i])
                     return false;
             }
 
             return true;
         }
+        #endregion
     }
 }
